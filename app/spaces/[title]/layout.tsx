b6cecf8d74d76d0425c008ce8fe819cd0e6eadbf
lawbhaiya <lawbhaix@gmail.com>
2025-11-30 04:01:55 +0530

    layout params fixed id-title

--- app/spaces/[title]/layout.tsx
+++ app/spaces/[title]/layout.tsx
@@ -1,6 +1,4 @@
 import { ReactNode } from "react";
-import { spaces } from "@/lib/courses";
-import App from "next/app";
 import AppSidebar from "@/components/sidebar/sidebar";
 
 export default function SpaceLayout({
@@ -8,13 +6,13 @@ export default function SpaceLayout({
   params,
 }: {
   children: ReactNode;
-  params: Promise<{ id: string }>;
+  params: { title: string };
 }) {
-    
   return (
-    <div className="flex w-full  min-h-screen min-w-0">
+    <div className="flex w-full min-h-screen min-w-0">
       {/* Sidebar */}
-        <AppSidebar/> 
+      <AppSidebar />
+
       {/* Main Content */}
       <main className="flex-1 min-h-screen w-full p-10 overflow-y-auto min-w-0">
         {children}
